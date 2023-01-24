891dd65af1a2d031325eaed17a83b368c366c6c6
hemax2000 <ibrahim.o.n@hotamil.com>
2023-01-24 11:56:47 +0300

    first commit

---
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./www s3://udagram-4095-0013-6750/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-4095-0013-6750/
